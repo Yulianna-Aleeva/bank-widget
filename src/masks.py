1fbc40c6a3ad6eca727c56d2bab8b09d68cd8a0d
Yulianna Aleeva <89673838388@mail.ru>
2026-04-20 23:38:26 +0500

    lesson_1

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Возвращает замаскированный номер банковской карты."""
+
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Возвращает замаскированный номер банковского счёта."""
+
+    return f"**{account_number[-4:]}"
