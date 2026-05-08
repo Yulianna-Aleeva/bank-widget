dd312259ee05fac4248a196a59ee78597836f0f1
Yulianna Aleeva <89673838388@mail.ru>
2026-05-08 16:02:51 +0500

    reserve copy "masks"

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,21 @@
+import string
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Возвращает замаскированный номер банковской карты."""
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    clean_card_number = "".join(char for char in card_number if char in string.digits)
+    if len(clean_card_number) == 0:
+        return "Номер отсутствует."
+    if len(clean_card_number) != 16:
+        return "Номер введён неверно."
+    return f"{clean_card_number[0:4]} {clean_card_number[4:6]}** **** {clean_card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Возвращает замаскированный номер банковского счёта."""
-    return f"**{account_number[-4:]}"
+    clean_account_number = "".join(char for char in account_number if char in string.digits)
+    if len(clean_account_number) == 0:
+        return "Номер отсутствует."
+    if len(clean_account_number) != 20:
+        return "Номер введён неверно."
+    return f"**{clean_account_number[-4:]}"
