921936be493dde0adf669a2556c62f7dcfd9c849
Yulianna Aleeva <89673838388@mail.ru>
2026-05-08 11:43:13 +0500

    Tests_1

--- src/widget.py
+++ src/widget.py
@@ -5,6 +5,15 @@ from src.masks import get_mask_card_number
 def mask_account_card(info: str) -> str:
     """Возвращает маскированные счёт или карту."""
 
+    if len(info) == 0:
+        return "Данные отсутствуют! Строка пустая."
+
+    if info.replace(" ", "").isalpha() or info.replace(" ", "").isdigit():
+        return "Отсутствует номер или наименование карты!"
+
+    if " " not in info:
+        return "Отсутствуют пробелы между номером и названием карты или счёта!"
+
     name, number = info.rsplit(" ", 1)
 
     if name.lower().startswith("счет"):
