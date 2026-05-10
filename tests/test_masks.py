c5ae587cf24a5758103f62f869a1ed18404880da
Yulianna Aleeva <89673838388@mail.ru>
2026-05-10 08:56:47 +0500

    refactor: test for masks, widget

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,14 +1,14 @@
-from src.masks import get_mask_card_number
 from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 
 
 # Запуск: pytest -k test_get_mask_card_number
-def test_get_mask_card_number(card_data):
-    card_number, expected_result = card_data
+def test_get_mask_card_number(mask_card_data: tuple) -> None:
+    card_number, expected_result = mask_card_data
     assert get_mask_card_number(card_number) == expected_result
 
 
 # Запуск: pytest -k test_get_mask_account
-def test_get_mask_account(account_data):
-    account_number, expected_result = account_data
+def test_get_mask_account(mask_account_data: tuple) -> None:
+    account_number, expected_result = mask_account_data
     assert get_mask_account(account_number) == expected_result
