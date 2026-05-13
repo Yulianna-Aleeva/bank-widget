78d55e1c87211b20a50ea9335866837d3629ee29
Yulianna Aleeva <89673838388@mail.ru>
2026-05-13 09:19:58 +0500

    refactor homework 10.2

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,11 +1,51 @@
+import pytest
+
 from src.masks import get_mask_account
 from src.masks import get_mask_card_number
+
+
+# Тесты для get_mask_card_number
+# Запуск: pytest -k test_get_mask_card_number_parametrize
+@pytest.mark.parametrize(
+    "card_number, expected_result",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("7000 7922 8960 6361", "7000 79** **** 6361"),
+        ("70*00а79w22/89ц60-63ф61", "7000 79** **** 6361"),
+        ("7000", "Номер введён неверно."),
+        ("7000792289606361111", "Номер введён неверно."),
+        ("Hello, World!", "Номер отсутствует."),
+        ("   ", "Номер отсутствует."),
+        ("", "Номер отсутствует."),
+    ],
+)
+def test_get_mask_card_number_parametrize(card_number: str, expected_result: str) -> None:
+    assert get_mask_card_number(card_number) == expected_result
 
 
 # Запуск: pytest -k test_get_mask_card_number
 def test_get_mask_card_number(get_mask_card_number_data: tuple) -> None:
     card_number, expected_result = get_mask_card_number_data
     assert get_mask_card_number(card_number) == expected_result
+
+
+# Тесты для get_mask_account
+# Запуск: pytest -k test_get_mask_account_parametrize
+@pytest.mark.parametrize(
+    "account_number, expected_result",
+    [
+        ("73654108430135874305", "**4305"),
+        ("73654 10843 01358 743 05", "**4305"),
+        ("в736+541,084ц301k358d743!05", "**4305"),
+        ("73654108", "Номер введён неверно."),
+        ("736541084301358743051111", "Номер введён неверно."),
+        ("Hello, World!", "Номер отсутствует."),
+        ("   ", "Номер отсутствует."),
+        ("", "Номер отсутствует."),
+    ],
+)
+def test_get_mask_account_parametrize(account_number: str, expected_result: str) -> None:
+    assert get_mask_account(account_number) == expected_result
 
 
 # Запуск: pytest -k test_get_mask_account
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,10 +2,31 @@ from typing import Any
 from typing import Dict
 from typing import List
 
+import pytest
+
 from src.processing import filter_by_state
 from src.processing import sort_by_date
 
 
+# Тесты для test_filter_by_state
+# Запуск: pytest -k test_filter_by_state_parametrize
+@pytest.mark.parametrize("state", ["EXECUTED", "CANCELED", "UNKNOWN", ""])
+def test_filter_by_state_parametrize(state: str) -> None:
+    operations_by_filter = [
+        {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    result = filter_by_state(operations_by_filter, state)
+    status_exists = any(item["state"] == state for item in operations_by_filter)
+    if status_exists:
+        assert len(result) > 0
+    else:
+        assert result == []
+
+
+# Запуск: pytest -k test_filter_by_state
 def test_filter_by_state(operations_data: List[Dict[str, Any]], filter_state: str) -> None:
     result_filtered = filter_by_state(operations_data, state=filter_state)
     state_exists = any(item.get("state") == filter_state for item in operations_data)
@@ -16,8 +37,30 @@ def test_filter_by_state(operations_data: List[Dict[str, Any]], filter_state: st
 
 
 # Тесты для sort_by_date
+# Запуск: pytest -k test_sort_by_date_parametrize
+@pytest.mark.parametrize(
+    "operations, reverse, expected",
+    [
+        # Проверка сортировки по убыванию
+        ([{"date": "2008-03-03"}, {"date": "2008-03-01"}], True, [{"date": "2008-03-03"}, {"date": "2008-03-01"}]),
+        # Проверка сортировки по возрастанию
+        ([{"date": "2008-03-03"}, {"date": "2008-03-01"}], False, [{"date": "2008-03-01"}, {"date": "2008-03-03"}]),
+        # Проверка дубликатов
+        ([{"date": "2008-03-03"}, {"date": "2008-03-03"}], True, [{"date": "2008-03-03"}, {"date": "2008-03-03"}]),
+        # Пустое значение даты
+        ([{"date": ""}, {"date": "2008-03-03"}], True, [{"date": "2008-03-03"}, {"date": ""}]),
+        # Пустой список
+        ([], True, []),
+    ],
+)
+def test_sort_by_date_parametrize(
+    operations: List[Dict[str, Any]], reverse: bool, expected: List[Dict[str, Any]]
+) -> None:
+    result = sort_by_date(operations, reverse)
+    assert sorted(result, key=lambda x: str(x)) == sorted(expected, key=lambda x: str(x))
 
 
+# Запуск: pytest -k test_sort_by_date_descending
 def test_sort_by_date_descending(sort_by_date_data: List[Dict[str, Any]]) -> None:
     """Проверка сортировки по убыванию."""
     result = sort_by_date(sort_by_date_data)
@@ -25,6 +68,7 @@ def test_sort_by_date_descending(sort_by_date_data: List[Dict[str, Any]]) -> Non
     assert dates == sorted(dates, reverse=True)
 
 
+# Запуск: pytest -k test_sort_by_date_ascending
 def test_sort_by_date_ascending(sort_by_date_data: List[Dict[str, Any]]) -> None:
     """Проверка сортировки по возрастанию."""
     result = sort_by_date(sort_by_date_data, reverse=False)
@@ -32,6 +76,7 @@ def test_sort_by_date_ascending(sort_by_date_data: List[Dict[str, Any]]) -> None
     assert dates == sorted(dates, reverse=False)
 
 
+# Запуск: pytest -k test_sort_by_date_double
 def test_sort_by_date_double(sort_by_date_double_data: List[Dict[str, Any]]) -> None:
     """Проверка дубликатов и сохранения списка."""
     result = sort_by_date(sort_by_date_double_data)
@@ -40,6 +85,7 @@ def test_sort_by_date_double(sort_by_date_double_data: List[Dict[str, Any]]) -> 
     assert len(double_ids) >= 2 and double_ids == sorted(double_ids)
 
 
+# Запуск: pytest -k test_sort_by_date_empty
 def test_sort_by_date_empty() -> None:
     """Пустой список."""
     assert sort_by_date([]) == []
